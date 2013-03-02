eb6044f46e78c5c7569e003769ce95143e6be8b7
chany229 <chany229@sina.com>
2013-03-02 17:58:51 +0800

    add tags with space

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,14 @@
 class Entry < ActiveRecord::Base
   acts_as_taggable
   acts_as_commentable
-  attr_accessible :body
+  
+  attr_accessible :body,:tags_with_space
+  attr_accessor :tags_with_space
+  
+  before_save :split_tags
+  def split_tags
+    if self.tags_with_space
+      self.tag_list = self.tags_with_space.split(" ").join(',')
+    end
+  end
 end
